769c9f4ec2851425fb3f65cfce09882f21af984d
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-01-23 17:55:15 +0500

    Add transaction velocity feature engineering

--- src/utils.py
+++ src/utils.py
@@ -177,3 +177,5 @@ def format_number(n: int) -> str:
 # WIP: tuning neural networks for fraud patterns [2026-01-22T14:02:05]
 
 # Update feature store integration for batch [2026-01-23T12:56:38]
+
+# Add transaction velocity feature engineering [2026-01-23T17:55:15]
